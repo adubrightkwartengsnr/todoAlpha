0481a77a60e28707f8001475f16f9b78c38205d4
Bright Adu Kwarteng snr <adubrightkwarrteng11@gmail.com>
2023-07-13 19:46:20 +0000

    added signup view and templates

--- Todo/Todo/settings.py
+++ Todo/Todo/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'main',
     'user',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- Todo/user/views.py
+++ Todo/user/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CustomUserRegistrationForm
 
 # Create your views here.
 def signup_view(request):
-    return "this is the signup page"
+    if request.method=="POST":
+        form = CustomUserRegistrationForm()
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.is_active=False
+            new_user.save()
+            return redirect('login')
+    else:
+        form = CustomUserRegistrationForm()      
+    return render(request,'user/signup.html',{"form":form})
+
+def login_view(request):
+    return render(request,'user.login.html')
