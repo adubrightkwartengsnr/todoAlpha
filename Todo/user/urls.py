da033c0a37b1f42a3fc58188b9927578044b9428
Bright Adu Kwarteng snr <adubrightkwarrteng11@gmail.com>
2023-07-17 02:37:36 +0000

    done with the user-authentication functionality

--- Todo/user/urls.py
+++ Todo/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as authviews
 urlpatterns = [
     path("signup/",views.signup_view,name = 'signup'),
-    path("login/",views.login_view,name="login"),
-    path("activate/<uidb64>/<token>/",views.activate,name='activate')
+    path("login/",views.CustomLoginView.as_view(template_name='user/login.html'),name="login"),
+    path("activate/<uidb64>/<token>/",views.activate,name='activate'),
 ]
